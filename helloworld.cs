5f481cb789816b76762ab974a773213a8c04d3dd
Hasnain Ali <beinghasnain16@gmail.com>
2020-05-21 22:57:17 +0500

    helloworld in C#

---
+++ helloworld.cs
@@ -0,0 +1,14 @@
+using System; 
+  
+namespace HelloWorldApp { 
+      
+    class Hello { 
+          
+        static void Main(string[] args) { 
+              
+            Console.WriteLine("Hello World!"); 
+
+            Console.ReadKey(); 
+        } 
+    } 
+} 
