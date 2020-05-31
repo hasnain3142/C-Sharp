a28ce58f29c0686d6b44632dde0c6f77594a6e49
Hasnain Ali <beinghasnain16@gmail.com>
2020-05-31 18:10:25 +0500

    Learning C#

---
+++ EvenOdd.cs
@@ -0,0 +1,15 @@
+using System; 
+  
+class EvenOdd
+{ 
+    public static void Main() 
+    { 
+        int n = 101; 
+
+        if(n % 2 == 0) 
+            Console.WriteLine("Even"); 
+        else
+            Console.WriteLine("Odd"); 
+    } 
+} 
+  
